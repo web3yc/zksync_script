a82d8aa25b24de575d11d31495e9cd5d8086566c
damien schlienger <43938720+damiano68@users.noreply.github.com>
2024-02-14 12:42:33 +0100

    Update getEraAndLiteNonce.js
    maybe is different

--- src/getEraAndLiteNonce.js
+++ src/getEraAndLiteNonce.js
@@ -1,41 +1,47 @@
-import { privates } from '../libs/getPrivates.js'
+import { privates } from '../libs/getPrivates.js';
 import * as zksync from "zksync";
 import * as ethers from "ethers";
 import * as zksyncWeb from "zksync-web3js";
 
-const syncProvider = await zksync.getDefaultProvider('mainnet');
-const ethersProvider = await ethers.getDefaultProvider('mainnet')
+const mainnet = 'mainnet';
+const zkSyncProviderUrl = 'https://mainnet.era.zksync.io';
 
-const zkSyncProvider = new zksyncWeb.Provider('https://mainnet.era.zksync.io');
-const ethProvider = ethers.getDefaultProvider('mainnet');
-
+const getProviders = async () => {
+    const syncProvider = await zksync.getDefaultProvider(mainnet);
+    const ethersProvider = await ethers.getDefaultProvider(mainnet);
+    const zkSyncProvider = new zksyncWeb.Provider(zkSyncProviderUrl);
+    const ethProvider = ethers.getDefaultProvider(mainnet);
+    
+    return { syncProvider, ethersProvider, zkSyncProvider, ethProvider };
+};
 
 const getNonce = async (privateKeys) => {
-    const lists = []
-    for (let index = 0; index < privateKeys.length; index++) {
-        const privateKey = privateKeys[index];
+    const { ethersProvider, syncProvider, zkSyncProvider, ethProvider } = await getProviders();
+    const lists = [];
+    
+    for (const privateKey of privateKeys) {
         const signer = new ethers.Wallet(privateKey, ethersProvider);
         const syncWallet = await zksync.Wallet.fromEthSigner(signer, syncProvider);
         const address = syncWallet.address();
-
-        const zkSyncWallet = new zksyncWeb.Wallet(
-            privateKey,
-            zkSyncProvider,
-            ethProvider
-        );
-        console.log(`查询中 Querying Address:${address}...`);
-        const [lite, era] = await Promise.all([syncWallet.getNonce(), zkSyncWallet.getNonce()])
+        
+        const zkSyncWallet = new zksyncWeb.Wallet(privateKey, zkSyncProvider, ethProvider);
+        console.log(`Querying Address:${address}...`);
+        const [lite, era] = await Promise.all([syncWallet.getNonce(), zkSyncWallet.getNonce()]);
+        
         console.table([{
             address,
             zksync_lite_nonce: lite,
             zksync_era_nonce: era,
         }]);
+        
         lists.push({
             account: address,
             zksync_lite_nonce: lite,
             zksync_era_nonce: era,
-        })
+        });
     }
-    console.table(lists)
-}
-getNonce(privates)
+    
+    console.table(lists);
+};
+
+getNonce(privates);
